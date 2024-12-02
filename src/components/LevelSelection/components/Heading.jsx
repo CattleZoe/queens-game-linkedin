4ab15dcffdd4f00b2ea851b8cf7ed2afd9cdcd9f
CattleZoe <5591163+CattleZoe@users.noreply.github.com>
2024-12-02 19:45:42 +0800

    Update Heading.jsx

--- src/components/LevelSelection/components/Heading.jsx
+++ src/components/LevelSelection/components/Heading.jsx
@@ -6,14 +6,9 @@ const Heading = () => {
     <div className="flex items-center space-x-3 mt-4 mb-6">
       <img src={queensLogo} width={32} height={32} alt="Queens Logo" />
       <h1 className="text-2xl">
-        <a
-          href="https://github.com/samimsu/queens-game-linkedin"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-700 font-medium hover:underline"
-        >
-          Queens
-        </a>
+       
+          Queen Never Cry Game
+        
       </h1>
     </div>
   );
