e2330ab2c13adccd3940cf28cb0c5045f75f3b7e
CattleZoe <5591163+CattleZoe@users.noreply.github.com>
2024-12-02 19:37:59 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,27 +4,15 @@ import GithubStarButton from "./GithubStarButton";
 const Footer = () => {
   return (
     <div className="px-2 space-y-3">
-      <!--
+      
       <div className="mx-auto w-fit flex items-center divide-x divide-slate-700 space-x-3">
-        <div>
-          Created by{" "}
-          <a
-            href="https://github.com/samimsu"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline font-medium"
-          >
-            Sami
-          </a>
-        </div>
+      
 
-        <Link to="/level-builder" className="pl-3 hover:underline">
-          Level Builder
-        </Link>
+        
       </div>
 
       <div className="mx-auto w-fit">
-        <GithubStarButton variant="small" />
+       
       </div>
       -->
     </div>
